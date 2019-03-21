1a4dd017bd17d28bdec841eda1eb252199413baa
Liyanda-g <Liyandacoelho95@gmail.com>
2019-03-21 19:41:39 +0530

    finsh

---
+++ arthm.sh
@@ -0,0 +1,8 @@
+echo "enter two numbers"
+read a b
+echo "$a+$b="`expr $a + $b`
+echo "$a-$b="`expr $a - $b`
+echo "$a*$b="`expr $a \* $b`
+echo "$a/$b="`expr $a / $b`
+echo "$a%$b="`expr $a % $b`
+
---
+++ large.sh
@@ -0,0 +1,9 @@
+echo " enter two numbers"
+read a b
+if [ $a -gt $b ]
+then
+echo "largest is $a"
+else
+echo "largest is $b"
+fi
+
